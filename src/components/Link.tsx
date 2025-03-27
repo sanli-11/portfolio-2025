6c05416fd22e083fcb9f1009dcbb744f57b0abdb
San Li <sanli.has11@gmail.com>
2025-03-27 19:10:33 +0500

    Broke down Header to a component and data file

---
+++ src/components/Link.tsx
@@ -0,0 +1,17 @@
+import type { linkType } from "../data/navLinks";
+
+function Link({ id, label, href, hasOverline = false, className }: linkType & { hasOverline?: boolean, className?: string }) {
+  const overline = "before:absolute before:left-1/2 before:top-0 before:h-1 before:w-0 before:bg-red-500 before:transition-all before:content-[''] hover:before:left-0 hover:before:w-full";
+
+  return (
+    <a
+      key={id}
+      href={href}
+      className={`relative block after:absolute after:bottom-0 after:left-1/2 after:h-1 after:w-0 after:bg-red-500 after:transition-all after:content-[''] hover:after:left-0 hover:after:w-full ${hasOverline && overline} ${className}`}
+    >
+      {label}
+    </a>
+  );
+}
+
+export default Link;
